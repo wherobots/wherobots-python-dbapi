a1145955ea129434c6793d68500c5a0205594935
Maxime Petazzoni <max@wherobots.com>
2024-04-12 12:32:49 -0700

    feat: export driver symbols

--- wherobots/db/__init__.py
+++ wherobots/db/__init__.py
@@ -1,1 +1,6 @@
+from .connection import Connection
+from .cursor import Cursor
 from .driver import connect, connect_direct
+from .errors import *
+from .region import Region
+from .runtime import Runtime
