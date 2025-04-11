8f5b1b1ef9723f5965eb859d0417c4dae63b7205
Maxime Petazzoni <max@wherobots.com>
2025-04-11 09:59:09 -0700

    feat: add from_name helpers to Runtime and Region

--- wherobots/db/region.py
+++ wherobots/db/region.py
@@ -4,3 +4,10 @@ from enum import Enum
 class Region(Enum):
     AWS_US_WEST_2 = "aws-us-west-2"
     AWS_EU_WEST_1 = "aws-eu-west-1"
+
+
+_NAME_TO_ENUM = {region.value: region for region in Region}
+
+
+def from_name(region: str) -> Region:
+    return _NAME_TO_ENUM[region]
