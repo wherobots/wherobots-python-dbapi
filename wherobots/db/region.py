6d236223a1bb5201f8067e7cb4bc570563c2310f
Maxime Petazzoni <max@wherobots.com>
2025-05-07 12:58:15 -0700

    feat: add us-east-1 compute region

--- wherobots/db/region.py
+++ wherobots/db/region.py
@@ -2,7 +2,11 @@ from enum import Enum
 
 
 class Region(Enum):
+    # Americas
+    AWS_US_EAST_1 = "aws-us-east-1"
     AWS_US_WEST_2 = "aws-us-west-2"
+
+    # EMEA
     AWS_EU_WEST_1 = "aws-eu-west-1"
 
 
