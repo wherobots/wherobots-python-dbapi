96794469ff0a4b25bdefc30f0077f1ad59f2fdc8
Maxime Petazzoni <max@wherobots.com>
2026-01-27 19:15:48 -0800

    chore: update available runtimes types

--- wherobots/db/runtime.py
+++ wherobots/db/runtime.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class Runtime(Enum):
+    # CPU/General purpose runtimes
     MICRO = "micro"
     TINY = "tiny"
     SMALL = "small"
@@ -11,18 +12,24 @@ class Runtime(Enum):
     XX_LARGE = "2x-large"
     XXXX_LARGE = "4x-large"
 
-    # HIMEM
+    # HIMEM/Memory-optimized runtimes
     MEDIUM_HIMEM = "medium-himem"
     LARGE_HIMEM = "large-himem"
     X_LARGE_HIMEM = "x-large-himem"
     XX_LARGE_HIMEM = "2x-large-himem"
     XXXX_LARGE_HIMEM = "4x-large-himem"
 
-    # GPU
+    # HICPU/Compute-optimized runtimes
+    X_LARGE_HICPU = "x-large-hicpu"
+    XX_LARGE_HICPU = "2x-large-hicpu"
+
+    # GPU-accelerated runtimes
     MICRO_A10_GPU = "micro-a10-gpu"
     TINY_A10_GPU = "tiny-a10-gpu"
     SMALL_A10_GPU = "small-a10-gpu"
     MEDIUM_A10_GPU = "medium-a10-gpu"
+    LARGE_A10_GPU = "large-a10-gpu"
+    X_LARGE_A10_GPU = "x-large-a10-gpu"
 
 
 _NAME_TO_ENUM = {runtime.value: runtime for runtime in Runtime}
